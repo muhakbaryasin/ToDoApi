5136a5ebc4ae54d187261a53a0e6e2fb297b97cc
muhammad akbar yasin <m.akbar.yasin@gmail.com>
2020-05-09 19:01:05 +0800

    Add other comments
    Signed-off-by: muhammad akbar yasin <m.akbar.yasin@gmail.com>

--- ToDoApi/Models/ToDosRepo.cs
+++ ToDoApi/Models/ToDosRepo.cs
@@ -10,31 +10,37 @@ namespace ToDoApi.Models
   {
     private readonly ToDoDbContext _context;
 
+    // constructur: passing ToDoDbContenxt
     public ToDosRepo( ToDoDbContext context )
     {
       _context = context;
     }
 
+    // Get all ToDo entries
     public DbSet<ToDo> Get()
     {
       return _context.ToDoItems;
     }
 
+    // Get ToDo by id
     public ToDo Get( int id )
     {
       return _context.ToDoItems.Find( id );
     }
 
+    // Get all ToDo entries of a certain date
     public IEnumerable<ToDo> Get( DateTime date )
     {
       return Get().Where( a => a.CreatedDate.Date == date.Date );
     }
 
+    // Get all ToDo entries of a certain date range
     public IEnumerable<ToDo> Get( DateTime dateStart, DateTime dateEnd )
     {
       return Get().Where( a => a.CreatedDate >= dateStart.Date && a.CreatedDate <= dateEnd.Date );
     }
 
+    // Insert new entry of ToDo
     public ToDo Insert( ToDo todo )
     {
       _context.ToDoItems.Add( todo );
@@ -43,12 +49,14 @@ namespace ToDoApi.Models
       return todo;
     }
 
+    // Update data of a ToDo 
     public void Update( ToDo todo )
     {
       _context.Entry( todo ).State = EntityState.Modified;
       _context.SaveChanges();
     }
 
+    // Update data completeness percentage of a ToDo by id
     public void Update( int id, int completenessPercentage )
     {
       var todo = Get( id );
@@ -58,6 +66,7 @@ namespace ToDoApi.Models
       _context.SaveChanges();
     }
 
+    // Delete a ToDo entry
     public void Delete( ToDo todo )
     {
       _context.ToDoItems.Remove( todo );
